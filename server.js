050bda14771178fce1d78772dca9d195c4d1ee7c
unknown <TFurman47@gmail.com>
2021-06-26 09:56:59 -0500

    for deployment

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
+//Fast, unopinionated, minimalist web framework for node.
 const express = require('express');
+//Node.js body parsing middleware
 const bodyParser = require('body-parser');
-const pg = require('pg-promise');
+//pg promise is a postgresSQL interface from Node.js
+const pgp = require('pg-promise');
+//internal command for node.js
 var http = require('http');
+//morgan is HTTP request logger middleware for node.js
 var morgan = require('morgan');
+//Calls database (need to ask how it knows which database to pull from);
+var dbsettings = process.env.DATABASE_URL || {database: ''};
+var db = pgp(dbsettings);
 
+//gives variables to express & http request for future use
 var app = express();
 var server = http.createServer(app);
+
 
 app.set('views', 'templates');
 app.set('view engine', 'html');
@@ -17,6 +27,8 @@ app.listen();
 
 app.use(express.static('public'));
 
-server.listen(3000, ()=>{
-    console.log('listening on *:3000');
-})
+//changed from 8000 to PORT for Heroku deployment
+var PORT = process.env.PORT || 8000;
+server.listen(PORT, ()=>{
+    console.log('listening on ', PORT);
+});
