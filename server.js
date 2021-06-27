b70eeb0e895641eb1033019015c2ef8e64615cd7
unknown <TFurman47@gmail.com>
2021-06-27 09:44:28 -0500

    update

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
-//Fast, unopinionated, minimalist web framework for node.
+//Web framework for node.
 const express = require('express');
 //Node.js body parsing middleware
-const bodyParser = require('body-parser');
+//process.env.DATABASE_URL || 
+
+const pgp = require('pg-promise')({});
+var dbsettings = pgp({database: 'restaurantreviewer'})
 //pg promise is a postgresSQL interface from Node.js
-const pgp = require('pg-promise');
+
+//*dotenv (to put secret passwords in, create .env file, then open file and put database_url (postgres://{user}:{password}@{hostname}:{port}/{database-name}) then put (Const env = require(‘dotenv’);) at the top of JS file
+
+
 //internal command for node.js
 var http = require('http');
 //morgan is HTTP request logger middleware for node.js
+
 var morgan = require('morgan');
 //Calls database (need to ask how it knows which database to pull from);
-var dbsettings = process.env.DATABASE_URL || {database: ''};
+
 var db = pgp(dbsettings);
 
 //gives variables to express & http request for future use
@@ -20,10 +27,10 @@ var server = http.createServer(app);
 app.set('views', 'templates');
 app.set('view engine', 'html');
 
-app.use(bodyParser.urlencoded());
+app.use(express.urlencoded());
 app.use('/public', express.static('public'));
 
-app.listen();
+//app.listen();
 
 app.use(express.static('public'));
 
@@ -31,4 +38,13 @@ app.use(express.static('public'));
 var PORT = process.env.PORT || 8000;
 server.listen(PORT, ()=>{
     console.log('listening on ', PORT);
+});
+
+app.get("/", async (req, res)=>{
+    var sets = await db.query('SELECT * FROM "review"');
+    console.log(sets);
+});
+
+app.get("/next", async(req,res)=>{
+    res = console.log("hello there");
 });
