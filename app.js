d9f89b1abab03132dda2a5b4ea6ca8f82dce8cc2
unknown <TFurman47@gmail.com>
2021-06-27 13:48:08 -0500

    es6 renderer udpate

--- app.js
+++ app.js
@@ -3,31 +3,31 @@ var http = require('http');
 var app = express();
 var server = http.createServer(app);
 
-
-app.use(express.static('public'));
+const es6Renderer = require('express-es6-template-engine');
+//registers the template engine function and associates it with html files
+app.engine('html', es6Renderer);
+//tell Express to look in the templates directory for the template ("view") files
+app.set('views', 'templates');
+//set the html template engine (our es6Renderer as the default for this application
+app.set('view engine', 'html');
 
 app.use(express.json());
 
+
+app.get('/', (req, res) => {
+    res.render('header');
+});
+
+
+app.use(express.static('public'));
 
 //calls the reviewer data from the routes folder once localhost:8000/reviewer is inputed into the URL
 const reviewer = require('./routes/reviewer')
 
 app.use('/reviewer', reviewer);
 
-
 //changed from 8000 to PORT for Heroku deployment
 var PORT = process.env.PORT || 8000;
 server.listen(PORT, ()=>{
     console.log('listening on ', PORT);
 });
-
-
-
-// app.get("/", async (req, res)=>{
-//     //var sets = await db.query('SELECT * FROM "review"');
-//     res = console.log("Howdy");
-// });
-
-// app.get("/next", async(req,res)=>{
-//     res = console.log("hello there");
-// });
