ad5f9d242c883241f8dbf23e1db167ba60ef0021
unknown <TFurman47@gmail.com>
2021-06-29 21:33:26 -0500

    update

--- app.js
+++ app.js
@@ -2,39 +2,44 @@ const express = require('express');
 var http = require('http');
 var app = express();
 var server = http.createServer(app);
-const bodyParser = require('body-parser');
 
 const es6Renderer = require('express-es6-template-engine');
 //registers the template engine function and associates it with html files
 app.engine('html', es6Renderer);
 //tell Express to look in the templates directory for the template ("view") files
-app.set('views', 'templates');
+app.set('views', 'public');
 //set the html template engine (our es6Renderer as the default for this application
 app.set('view engine', 'html');
 
 //Body Parser
-
-
+app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
 
 app.get('/', (req, res) => {
-    res.render('header');
+    res.render('index');
 });
 
-app.get('/?', (req,res)=>{
-    let term = req.query.searchTerm;
-    console.log('Term: ', term);
-    db
+app.post('/search', (req, res) => {
+    const {search} = req.body;
+    console.log (search);
+    res.render('searchpage');
 });
 
 
 app.use(express.static('public'));
 
-//calls the reviewer data from the routes folder once localhost:8000/reviewer is inputed into the URL
-const reviewer = require('./routes/reviewer')
+//calls the database from the routes folder once localhost:8000/*** is inputed into the URL
+const search = require('./routes/search')
+app.use('/search', search);
 
+// const restaurant = require('./routes/restaurant')
+// app.use('/restaurant', restaurant);
+
+const reviewer = require('./routes/reviewer')
 app.use('/reviewer', reviewer);
+
+
 
 //changed from 8000 to PORT for Heroku deployment
 var PORT = process.env.PORT || 8000;
---
+++ routes/search.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../db');
+
+router.get('/:name', (req, res) =>{
+    console.log(req.params);
+    db.any(`SELECT * FROM restaurant WHERE name ILIKE '${req.params.name}';`)
+    .then(rows =>{
+        console.log(rows);
+        res.render('searchpage');
+        console.log("found me")
+    
+        for (var i = 0; i < rows.length; i++) {
+             var row = rows[i];
+             console.log(row.name);
+             console.log(row.address); 
+         }
+             
+    })
+    .catch(error => {
+        error = console.log("Bad");
+    })
+})
+
+module.exports = router;
